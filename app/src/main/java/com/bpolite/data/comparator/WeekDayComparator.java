9e90cf5e351e54d0c5a55becb3eeddbe7645f317
Yuri Granovsky <yurig@perfectomobile.com>
2017-02-11 14:12:37 +0200

    update WeekDayComparator

--- app/src/main/java/com/bpolite/data/comparator/WeekDayComparator.java
+++ app/src/main/java/com/bpolite/data/comparator/WeekDayComparator.java
@@ -6,42 +6,56 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.Locale;
+import java.util.List;
 
 public class WeekDayComparator implements Comparator<WeekDay> {
 
-	private static LinkedList<WeekDay> weekDaysByLocale = null;
-	private static WeekDay[] days = WeekDay.values();
-	private static int firstDayOfWeekIndex = Calendar.getInstance(Locale.getDefault()).getFirstDayOfWeek();
+    private static WeekDay[] weekDays = WeekDay.values();
+    private int firstDayOfWeekIndex = Calendar.getInstance().getFirstDayOfWeek() - 1;
 
-	@Override
-	public int compare(WeekDay wd1, WeekDay wd2) {
-		if (wd1.equals(wd2)) {
-			return 0;
-		}
+    public WeekDayComparator() {
+        firstDayOfWeekIndex = Calendar.getInstance().getFirstDayOfWeek() - 1;
+    }
 
-		if (wd1.equals(days[firstDayOfWeekIndex - 1])) {
-			return -1;
-		}
+    /**
+     *
+     * @param firstDayOfWeekIndex 0 = Sun, 1 = Mon ...
+     */
+    public WeekDayComparator(int firstDayOfWeekIndex) {
+        this.firstDayOfWeekIndex = firstDayOfWeekIndex;
+    }
 
-		if (wd2.equals(days[firstDayOfWeekIndex - 1])) {
-			return 1;
-		}
 
-		if (weekDaysByLocale == null) {
-			weekDaysByLocale = new LinkedList<>(Arrays.asList(days));
-			for (int i = 0; i < firstDayOfWeekIndex - 1; i++) {
-				weekDaysByLocale.addLast(weekDaysByLocale.removeFirst());
-			}
-		}
+    @Override
+    public int compare(WeekDay wd1, WeekDay wd2) {
+        if (wd1.equals(wd2)) {
+            return 0;
+        }
 
-		for (WeekDay weekDay : weekDaysByLocale) {
-			if (weekDay.equals(wd1))
-				return -1;
-			else if (weekDay.equals(wd2))
-				return 1;
-		}
+        if (wd1.equals(weekDays[firstDayOfWeekIndex])) {
+            return -1;
+        }
 
-		return 0;
-	}
+        if (wd2.equals(weekDays[firstDayOfWeekIndex])) {
+            return 1;
+        }
+
+        for (WeekDay weekDay : getWeekDaysByLocale(firstDayOfWeekIndex)) {
+            if (weekDay.equals(wd1))
+                return -1;
+            else if (weekDay.equals(wd2))
+                return 1;
+        }
+
+        return 0;
+    }
+
+    protected List<WeekDay> getWeekDaysByLocale(int firstDayOfWeekIndex) {
+        LinkedList<WeekDay> result;
+        result = new LinkedList<>(Arrays.asList(weekDays));
+        for (int i = 0; i < firstDayOfWeekIndex; i++) {
+            result.addLast(result.removeFirst());
+        }
+        return result;
+    }
 }
